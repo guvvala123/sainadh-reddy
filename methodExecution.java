ae682538794dbab8a7932220f9a5051caaae8674
guvvala123 <97467810+guvvala123@users.noreply.github.com>
2022-01-19 17:23:10 +0530

    Add files via upload

---
+++ methodExecution.java
@@ -0,0 +1,19 @@
+package sainadh;
+
+public class methodExecution {
+
+		//method demo
+		public int multipynumbers(int a,int b) {
+			int z=a*b;
+			return z;
+		}
+
+		public static void main(String[] args) {
+
+			methodExecution b=new methodExecution();
+			int ans= b.multipynumbers(20,3);
+			System.out.println("Multipilcation is :"+ans);
+			}
+		}
+
+		
---
+++ overloadMethod.java
@@ -0,0 +1,26 @@
+package sainadh;
+
+		//method overloading
+		public class overloadMethod {
+			
+		public void area(int b,int h)
+		    {
+		         System.out.println("Area of Triangle : "+(0.5*b*h));
+		    }
+		    public void area(int r) 
+		    {
+		         System.out.println("Area of Circle : "+(3.14*r*r));
+		    }
+
+		    public static void main(String args[])
+		   {
+
+		overloadMethod ob=new overloadMethod();
+		       ob.area(15,20);
+		       ob.area(7);  
+		   }
+		}
+
+
+
+
