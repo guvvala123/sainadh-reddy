9edb755c7074d526745e8ca9b3d7cd3db4e068ae
guvvala123 <97467810+guvvala123@users.noreply.github.com>
2022-01-27 23:28:04 +0530

    Add files via upload

---
+++ practise project/EmailValidate.java
@@ -0,0 +1,28 @@
+package sainadh;
+import java.util.*;
+		public class EmailValidate {
+        public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				
+				System.out.println("Enter the email to search: ");
+				Scanner scan = new Scanner(System.in);
+				String name = scan.next(); 
+				
+				ArrayList <String> list = new ArrayList<String>();
+				list.add("sainadh@gmail.com");
+			    list.add("lakshmi@gmail.com");
+			    list.add("ramya@domain.com");
+			    list.add("vamsi@domain.com");
+			    list.add("venky@gmail.com");
+			    
+			    System.out.println("Search Results: ");
+			    for (String element : list) {
+			       if (element.contains(name)) {
+			             System.out.println(element + " " + "is present in the array!");
+			              }  
+			        }
+			    scan.close();
+			     }
+			}
+
+	
